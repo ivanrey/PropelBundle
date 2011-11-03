4e0b0512ee1107948ba44b5fbc763af490a2838b
William DURAND <william.durand1@gmail.com>
2011-11-03 22:22:58 +0100

    Moved some classes to the Symfony2 core (Propel Bridge)

--- Controller/PanelController.php
+++ Controller/PanelController.php
@@ -10,9 +10,8 @@
 
 namespace Propel\PropelBundle\Controller;
 
+use Symfony\Bridge\Propel\DataCollector\PropelDataCollector;
 use Symfony\Component\DependencyInjection\ContainerAware;
-
-use Propel\PropelBundle\DataCollector\PropelDataCollector;
 
 /**
  * PanelController is designed to display information in the Propel Panel.
